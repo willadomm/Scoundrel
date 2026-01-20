ace86f3e440270731908a9150a4f9cacbbe054b4
willadomm <wdomm@oberlin.edu>
2026-01-20 15:46:55 -0800

    putting the finishing touches on

--- gameloop.py
+++ gameloop.py
@@ -11,6 +11,7 @@ class Gamestate:
         self.prevroomskipped = False
         self.room = []
         self.healthpotionused = False
+        self.roomcounter = 0
 
         self.gameloop()
     
@@ -39,10 +40,16 @@ class Gamestate:
 
     def fight(self, cardselection):
         if len(self.weapon) > 0:
-            fightbool = input("Would you like to fight this monster with your equipped weapon?(Press y for yes): ")
-            if fightbool == "y" or fightbool == "Y":
-                self.fightwithweapon(cardselection)
-            else:
+            if self.weapon[1] > card.Card.getDigit(cardselection):
+                fightbool = input("Would you like to fight this monster with your equipped weapon?(Press y for yes): ")
+                if fightbool == "y" or fightbool == "Y":
+                    self.fightwithweapon(cardselection)
+                else:
+                    self.fightbarehanded(cardselection)
+            else: 
+                print()
+                time.sleep(0.5)
+                print("Your weapon cannot be used against this monster.")
                 self.fightbarehanded(cardselection)
         else:
             self.fightbarehanded(cardselection)
@@ -54,10 +61,48 @@ class Gamestate:
         print("You are now at " + str(self.life) + " health")
 
     def fightwithweapon(self, cardselection):
-        if (card.Card.getDigit(self.weapon[0])) - card.Card.getDigit(cardselection) > 0:
-            print("You took no damage from the monster, as your weapon was a hogher power.")
+        if (card.Card.getDigit(self.weapon[0])) - card.Card.getDigit(cardselection) >= 0:
+            print("You took no damage from the monster, as your weapon was a higher power.")
+            time.sleep(1)
             print("Your weapon can now only be used to slay monsters of less strength than " + str(card.Card.getDigit(cardselection)))
+            self.weapon[1] = card.Card.getDigit(cardselection)
+        else:
+            self.life = self.life - (card.Card.getDigit(cardselection) - card.Card.getDigit(self.weapon[0]))
+            print("You took " + str(card.Card.getDigit(cardselection) - card.Card.getDigit(self.weapon[0])) + " damage from the monster. ")
+            time.sleep(1)
+            print("Your life is now " + str(self.life))
+    
 
+    def constructroomdetails(self):
+        print()
+        print("Life: " + str(self.life))
+        time.sleep(0.5)
+        print()
+        if len(self.weapon) > 0:
+            print("Current Weapon: " + card.Card.display(self.weapon[0]))
+            if self.weapon[1] < 20:
+                print("Can only be used against monsters with power less than " + str(self.weapon[1]))
+            print()
+
+        
+        time.sleep(0.5)
+        print("Room number: " + str(self.roomcounter))
+        print()
+        print("Cards left in deck: " + str(len(self.deck)))
+        print()
+        
+
+
+
+    def lifetotalcheck(self, cardselect):
+        if self.life < 1:
+            print()
+            print("Game Over! You bled out in the dungeon.")
+            print()
+            print("You were killed by: " + card.Card.display(cardselect))
+            print() 
+            print("Cards remaining in deck: " + str(len(self.deck)))
+            exit()
 
 
 
@@ -69,33 +114,69 @@ class Gamestate:
         self.room = self.deck[0:4]
         self.deck = self.deck[4:]
         self.healthpotionused = False
-    
-        while len(self.room) > 1:
+        self.roomcounter += 1
+
+        time.sleep(0.5)
+
+        self.constructroomdetails()
+
+
+
+        time.sleep(0.5)
+
+        if self.prevroomskipped == False and len(self.room) == 4:
             counter = 1
             for i in self.room:
                 print(str(counter) + ". " + card.Card.display(i))
                 counter += 1
-            if self.prevroomskipped == False and len(self.room) == 4:
-                skipask = "n"
-                skipask = input("Since you haven't skipped the previous room, you may skip this one. Would you like to do this? (Press s to skip, and press anything else to not skip): ")
-                if skipask == "s":
-                    self.discard.extend(self.room)
-                    self.prevroomskipped = True
-                    self.gameloop()
+            skipask = "n"
+            print("Since you haven't skipped the previous room, you may skip this one. Would you like to do this? ")
+            skipask = input("Press s to skip, or press the card number to choose that card: ")
+            if skipask == "s":
+                self.discard.extend(self.room)
+                self.prevroomskipped = True
+                self.room = []
+        else:
+            skipask = "n"
+
+            
+
+        while len(self.room) > 1:
             
             self.prevroomskipped = False
-            
+
             cardselect = 0
 
-            while cardselect < 1 or cardselect > 4:
-                cardselect = int(input("Which card would you like to choose first?: "))
-                if cardselect < 1 or cardselect > 4:
-                    print("Please select a number from 1-4")
-    
-        
-            self.select(self.room[cardselect-1])
+            if skipask.isnumeric() and int(skipask)> 0 and int(skipask) <5 :
+                cardselect = skipask
 
-            self.discard.append(self.room.pop(cardselect-1))
+            else:
+                counter = 1
+                for i in self.room:
+                    print(str(counter) + ". " + card.Card.display(i))
+                    counter += 1
+                while cardselect < 1 or cardselect > len(self.room):
+                    cardselect = int(input("Which card would you like to choose first?: "))
+                    if cardselect < 1 or cardselect > len(self.room):
+                        print("Please select a number from 1-4")
+                
+    
+            skipask = "n"
+
+            self.select(self.room[(int(cardselect)-1)])
+
+            self.discard.append(self.room.pop(int(cardselect)-1))
+
+
+
+           
+
+        if (len(self.room) > 0):
+            self.deck.insert(0, self.room[0])
+
+        self.gameloop()
+        
+        
 
  
         
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import setup
 import gameloop
+import time
 
 def main():
+    userinput = input("Hello! Welcome to Scoundrel. Press enter to start a new game: ")
+    time.sleep(0.5)
+    print("Shuffling Deck...")
+    time.sleep(0.5)
     deck = setup.setup()
     gameloop.Gamestate(deck)
 
